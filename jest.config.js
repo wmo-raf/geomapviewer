1cefb066f8bf47cf066c2ab41cc371b6a4d363dc
FGG Consultant <florent.gravin@gmail.com>
2026-04-07 15:25:58 +0200

    add jest tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+module.exports = createJestConfig({
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+});
